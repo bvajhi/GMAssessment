623b3162f7c5f2e9fa2b54e78d21e3e94c2397fb
bvajhi <bvajhi@gmail.com>
2020-10-29 08:27:53 -0500

    Added required constants

--- constants/index.js
+++ constants/index.js
@@ -2,6 +2,8 @@ import keyMirror from 'keymirror'
 
 export default CONST = keyMirror({
     COUNTER_CHANGE: null,
-
+    GET_CURRENT_REPO_COMMITS: null,
+    SET_LOADER: null,
+    GET_CURRENT_REPO_COMMITS_FAIL: null,
 
 });
