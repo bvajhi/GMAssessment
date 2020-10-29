4debe3e35e385c9d835a299ade6fd536fb336589
bvajhi <bvajhi@gmail.com>
2020-10-28 20:58:57 -0500

    Finishesd setting up redux

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 /**
  * @format
  */
-
+import React from 'react';
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+import { Provider } from 'react-redux';
+
+import configureStore from './store/configureStore';
+
+const ReduxApp = () => (
+    <Provider store={configureStore()}>
+        <App/>
+    </Provider>
+)
+
+
+
+
+AppRegistry.registerComponent(appName, () => ReduxApp);
+
